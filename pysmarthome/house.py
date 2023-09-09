8b39da109a41453ae6653f070e15c1b6d2e3695e
Olivier Staquet <olivier.staquet@gabismartcare.com>
2023-09-09 15:32:50 +0200

    ⚙️ Fix CI based in ruff feedback

--- pysmarthome/house.py
+++ pysmarthome/house.py
@@ -4,4 +4,20 @@ from pysmarthome.room import Room
 
 class House:
     def __init__(self):
-        rooms: List[Room] = []
+        self.rooms: List[Room] = [Room("Living room")]
+
+    def print_debug_status(self):
+        for room in self.rooms:
+            print("Status in " + room.get_name())
+            print("  Temperature " + str(room.get_temperature()) + "°C")
+            status:str = "Windows "
+            if room.window_opened:
+                status = status + "opened"
+            else:
+                status = status + "closed"
+            status = status + ", heating system is "
+            if room.heating_enabled:
+                status = status + "ON"
+            else:
+                status = status + "OFF"
+            print("  " + status)
